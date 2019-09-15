f5f44991928de380348189eb685c3564d81e6956
Divya <enjamuridivya@gmail.com>
2019-09-15 14:47:03 -0700

    server page

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+// var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(express.json());
+app.use(express.json({ type: 'application/vnd.api+json' }));
+
+//This is to use my CSS
+app.use(express.static('app/public'));
+
+//returning the function and the additional parantheses are calling the function
+require('./app/routing/htmlroutes')(app);
+require('./app/routing/apiroutes')(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
